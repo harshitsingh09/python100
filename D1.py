67f31667d3a703e932b78be6314b36be357339fb
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-07-01 05:41:50 +0000

    complete D1

---
+++ D1.py
@@ -0,0 +1,11 @@
+# This is the BAND NAME GENERATOR
+print("This is the band Name Generator")
+
+# takes string input from user
+city = input("Name the city you grew up in:\n")
+
+# takes string input from user
+pet = input("What is your pet's name?\n")
+
+# displays the output string by contatenating the city and pet variable strings
+print("Your band name could be " + city + " " + pet)
