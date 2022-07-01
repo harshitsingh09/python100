4602e0fe36778fe7a0f3476449e24226539fe70f
Harshit Singh <89565061+harshitsingh09@users.noreply.github.com>
2022-07-01 05:56:20 +0000

    complete D2.1

---
+++ D2.1.py
@@ -0,0 +1,21 @@
+# DIGIT ADDITION
+print("Welcome to the digit addition calculator")
+
+# takes input from user
+num = input("Enter a two digit interger!\n")
+
+# find type of variable num
+print(type(num))
+
+# display first digit
+print(num[0])
+
+# display second digit
+print(num[1])
+
+# Adding both values and storing in variable num
+# Converting datatype of num values from string to integer
+sum = int(num[0]) + int(num[1])
+
+# displaying sum output
+print("The sum of digits in 2 digit integer =", sum)
