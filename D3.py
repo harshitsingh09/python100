bb3b6ff14b69d949650d43464d1308bd7aa9d7ff
harshitsingh09 <harshit.arvind@gmail.com>
2022-07-11 22:31:45 +0530

    work on D3 project

---
+++ D3.py
@@ -0,0 +1,9 @@
+# Treasure Island
+# A game made using If..elif..else statements
+
+# Greetings
+print('Welcome to TREASURE ISLAND!\n Your mission is to find the Hidden Treasure CHEST')
+
+# Sequence of events
+p1 = input('You have spawned at a cross-road! Choose one road to travel on.. \n[type "left" or "right"]')
+if p1 == 'left':
