0df0dc3bb333b04486aae307d7b2ddf64c37ede3
harshitsingh09 <harshit.arvind@gmail.com>
2022-07-19 09:14:12 +0530

    Heads or Tails

--- D4.1.py
+++ D4.1.py
@@ -1,7 +1,17 @@
-# 
-#
+# Using the Random module
 
-# Greetings
-print('Welcome to the ')
+# Importing the random module
+import random
 
-#
+# Entering seed value
+test_seed = int(input("Create a seed number: "))
+random.seed(test_seed)
+
+# Entering a random integer into random_side variable
+random_side = random.randint(0,1)
+
+# Using conditional statements to print desired output
+if random_side == 1:
+    print('Heads')
+else:
+    print('Tails')
